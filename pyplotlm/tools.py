0b5442a80a346c33ecc780b64663e03b9e6a1b22
esmondhkchu <chuhke@gmail.com>
2020-07-22 15:55:22 -0700

    added docstring to tools.py

--- pyplotlm/tools.py
+++ pyplotlm/tools.py
@@ -12,7 +12,14 @@ class DimensionError(Error):
     pass
 
 def abline(intercept, slope, x_min=0, x_max=10, marker=':', color='black'):
-    """Plot a line from slope and intercept
+    """ plot a line from slope and intercept
+
+    Parameters: intercept (float) - intercept
+                slope (float) - slope
+                x_min (float) - x minimum value, optional, default is 0
+                x_max (float) - x maximum value, optional, default is 10
+                marker (str) - matplotlib marker
+                color (str) - line color
     """
     x_vals = np.linspace(x_min, x_max)
     y_vals = x_vals*slope + intercept
